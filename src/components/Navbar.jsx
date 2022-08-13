a4fbe7dfa2069d093fccae123e7ea3490425ca0e
Oforduhh <oforduhh@gmail.com>
2022-08-13 13:58:37 +0100

    styling navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,71 @@
-import { AppBar, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  InputBase,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { styled } from "@mui/system";
 import React from "react";
+import MailIcon from "@mui/icons-material/Mail";
+import Badge from "@mui/material/Badge";
+import { Pets, Notifications } from "@mui/icons-material";
+import { deepOrange } from "@mui/material/colors";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+  //   border: "2px solid red",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  color: "#fff",
+  display: "flex",
+  gap: "20px",
+  border:"3px solid red",
+  alignItems:"center"
+}));
 
 const Navbar = () => {
   return (
     <AppBar position="stick" p={2}>
-      <Toolbar>Navbar</Toolbar>
+      <StyledToolbar>
+        <Typography
+          variant="h6"
+          sx={{
+            display: { xs: "none", sm: "block" },
+          }}
+        >
+          HARRISON DEV
+        </Typography>
+        <Pets
+          sx={{
+            display: { xs: "block", sm: "none" },
+          }}
+        />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar sx={{ bgcolor: deepOrange[500], width: 30, height: 30 }}>
+            N
+          </Avatar>
+        </Icons>
+      </StyledToolbar>
     </AppBar>
   );
 };
--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { Box } from "@mui/material";
 const Rightbar = () => {
-  return <Box sx={{ flex: "1", border: "2px solid red" }} p={2}>Rightbar</Box>;
+  return (
+    <Box
+      sx={{
+        flex: "1",
+        border: "2px solid red",
+        display: { xs: "none", sm: "block" },
+      }}
+      p={2}
+    >
+      Rightbar
+    </Box>
+  );
 };
 
 export default Rightbar;
