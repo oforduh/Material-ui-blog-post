2784c90d8d703640f3c0f77cfc65b001b2fa69d6
Oforduhh <oforduhh@gmail.com>
2022-08-14 22:50:09 +0100

    worked on the images

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -81,7 +81,7 @@ const Post = () => {
           }}
           component="img"
           height="15%"
-          image="https://images.pexels.com/photos/4534200/pexels-photo-4534200.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          image="https://source.unsplash.com/1600x900/?fitness"
           alt="Paella dish"
         />
         <CardContent>
@@ -124,7 +124,7 @@ const Post = () => {
           }}
           component="img"
           height="15%"
-          image="https://images.pexels.com/photos/4534200/pexels-photo-4534200.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          image="https://source.unsplash.com/1600x900/?sport"
           alt="Paella dish"
         />
         <CardContent>
@@ -167,7 +167,7 @@ const Post = () => {
           }}
           component="img"
           height="15%"
-          image="https://images.pexels.com/photos/4534200/pexels-photo-4534200.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          image="https://source.unsplash.com/1600x900/?animal"
           alt="Paella dish"
         />
         <CardContent>
@@ -210,7 +210,7 @@ const Post = () => {
           }}
           component="img"
           height="15%"
-          image="https://images.pexels.com/photos/4534200/pexels-photo-4534200.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          image="https://source.unsplash.com/1600x900/?food"
           alt="Paella dish"
         />
         <CardContent>
@@ -232,6 +232,49 @@ const Post = () => {
           </IconButton>
         </CardActions>
       </Card>
+      <Card sx={{ margin: 3 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        sx={{
+          height: { sm: 450 },
+        }}
+        component="img"
+        height="15%"
+        image="https://source.unsplash.com/1600x900/?art"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to
+          cook together with your guests. Add 1 cup of frozen peas along with
+          the mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <Checkbox
+            icon={<FavoriteBorder />}
+            checkedIcon={<Favorite sx={{ color: "red" }} />}
+          />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
     </>
   );
 };
